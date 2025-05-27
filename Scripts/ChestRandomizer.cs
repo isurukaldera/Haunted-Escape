c6d467c1191ce289f5c2b73c618f3d61ecbd1f63
isurukaldera <75193561+isurukaldera@users.noreply.github.com>
2025-05-27 20:51:58 +0300

    Add files via upload

---
+++ Scripts/ChestRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChestRandomizer : MonoBehaviour
+{
+public GameObject chest; // Drag your chest GameObject here in the inspector
+public Transform[] spawnPoints; // Drag your spawn points here
+
+void Start()
+{
+// Choose a random spawn point
+int randomIndex = Random.Range(0, spawnPoints.Length);
+
+// Move the chest to that point
+chest.transform.position = spawnPoints[randomIndex].position;
+chest.transform.rotation = spawnPoints[randomIndex].rotation;
+}
+}
